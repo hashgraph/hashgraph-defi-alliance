46dde4c57d8e31959131c1ec07d9bec48d5267b7
Valeriu-Vladut Oprisor <vlad.oprisor95@gmail.com>
2024-03-07 11:23:32 +0200

    Fix tokens balance response limitation
    Signed-off-by: Valeriu-Vladut Oprisor <vlad.oprisor95@gmail.com>

--- hashgraph-react-wallets/src/actions/types.ts
+++ hashgraph-react-wallets/src/actions/types.ts
@@ -1,3 +1,7 @@
+export type MirrorResponseStatus = {
+  messages: { message: string }[]
+}
+
 export type MirrorBalancesTokenType = {
   token_id: string
   balance: number
@@ -15,7 +19,20 @@ export type MirrorBalancesResponse = {
     next: string | null
   }
   timestamp: string
-  _status?: {
-    messages: { message: string }[]
+  _status?: MirrorResponseStatus
+}
+
+export type MirrorTokensResponse = {
+  tokens: {
+    automatic_association: boolean
+    balance: number
+    created_timestamp: string
+    freeze_status: string
+    kyc_status: string
+    token_id: string
+  }[]
+  links: {
+    next: string | null
   }
+  _status?: MirrorResponseStatus
 }
